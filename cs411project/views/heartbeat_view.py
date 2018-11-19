acacfe5ba3b2a0cd3af4186170fbbc6ffb945a48
Hop <ltyihop@gmail.com>
2018-11-19 15:56:04 -0600

    internal error2

--- cs411project/views/heartbeat_view.py
+++ cs411project/views/heartbeat_view.py
@@ -6,11 +6,11 @@ from ..database.entity_serializer import EntitySerializer
 import json
 
 class InsertHB(MethodView):
-        def get(self,MachineID):
+        def get(self,machineID):
                 connection  = g.mysql_connection.get_connection()
                 cursor = connection.cursor(prepared=True)
                 query = "SELECT * FROM Machine WHERE Machine.MachineID = %s"
-                cursor.execute(query,(MachineID))
+                cursor.execute(query,(int(machineID)))
                 col_names = [x[0] for x in cursor.description]
                 result_as_dicts = list(EntitySerializer.db_entities_to_python(cursor, col_names))
                 cursor.close()
