5c718848054fd9ba1381222e31bf7bc5702d72aa
Hop <ltyihop@gmail.com>
2018-11-19 15:48:01 -0600

    internal error2

--- cs411project/views/heartbeat_view.py
+++ cs411project/views/heartbeat_view.py
@@ -1,17 +1,16 @@
-from flask import g
-from flask.views import MethodView
+from flask import g, Flask, request
 from flask.json import jsonify
+from flask.views import MethodView
 from ..database.entity_serializer import EntitySerializer
 
 import json
 
-class InsertHB(MethodView):
-
-    def get(self, machineID):
+class SpecificMachineView(MethodView):
+    def get(self,MachineID):
         connection  = g.mysql_connection.get_connection()
         cursor = connection.cursor(prepared=True)
         query = "SELECT * FROM Machine WHERE Machine.MachineID = %s"
-        cursor.execute(query,(machineID))
+        cursor.execute(query,(MachineID))
         col_names = [x[0] for x in cursor.description]
         result_as_dicts = list(EntitySerializer.db_entities_to_python(cursor, col_names))
         cursor.close()
