238deda7ebe6b426e9bc1f63cb1a9c7176f7a49f
Hop <ltyihop@gmail.com>
2018-11-19 10:29:14 -0600

    tab error

--- cs411project/views/heartbeat_view.py
+++ cs411project/views/heartbeat_view.py
@@ -17,7 +17,7 @@ class InsertHB(MethodView):
         #query = 'INSERT INTO HeartbeatSequence(Tfail, NumHeartBeats, NetID, MachineID, FirstTS, LastTS) VALUES (%s, 1, %s, %s, SUBTIME(CURRENT_TIMESTAMP, %s), SUBTIME(CURRENT_TIMESTAMP,%s));'
         #query_args = ('00:05:00',netID,machineID, '00:10:00', '00:09:00')
 	
-	   query =  """
+        query = """
                 SELECT NetID, FirstTS, LastTS, Tfail, SeqID
                 FROM HeartbeatSequence
                 WHERE SeqID =
