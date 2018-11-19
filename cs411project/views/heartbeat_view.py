ce44d1144619a52793dd2581852434065408f1fd
Hop <ltyihop@gmail.com>
2018-11-19 15:48:56 -0600

    internal error2

--- cs411project/views/heartbeat_view.py
+++ cs411project/views/heartbeat_view.py
@@ -6,12 +6,12 @@ from ..database.entity_serializer import EntitySerializer
 import json
 
 class SpecificMachineView(MethodView):
-    def get(self,MachineID):
-        connection  = g.mysql_connection.get_connection()
-        cursor = connection.cursor(prepared=True)
-        query = "SELECT * FROM Machine WHERE Machine.MachineID = %s"
-        cursor.execute(query,(MachineID))
-        col_names = [x[0] for x in cursor.description]
-        result_as_dicts = list(EntitySerializer.db_entities_to_python(cursor, col_names))
-        cursor.close()
-        return jsonify(result_as_dicts)
+        def get(self,MachineID):
+                connection  = g.mysql_connection.get_connection()
+                cursor = connection.cursor(prepared=True)
+                query = "SELECT * FROM Machine WHERE Machine.MachineID = %s"
+                cursor.execute(query,(MachineID))
+                col_names = [x[0] for x in cursor.description]
+                result_as_dicts = list(EntitySerializer.db_entities_to_python(cursor, col_names))
+                cursor.close()
+                return jsonify(result_as_dicts)
