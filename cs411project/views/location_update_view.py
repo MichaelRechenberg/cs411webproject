6f2bfd84d0668bb680085f3022526b59f08e8505
Michael Rechenberg <michaelrechenberg@oawl.net>
2018-11-29 21:27:28 -0600

    Some changes so getting machine availability and posting machine locations works..moving to cPanel

--- cs411project/views/location_update_view.py
+++ cs411project/views/location_update_view.py
@@ -18,15 +18,20 @@ class LocationView(MethodView):
     cursor = connection.cursor(prepared=True)
     cursor.execute("SELECT * FROM Users WHERE NetID = %s AND isTA = 1", (NetID,))
     result = list(cursor)
+
+    # User is a TA, allow the location change to go through
     if result:
+      delete_all_previous_locations_query = "DELETE FROM MachineLocation"
+      cursor.execute(delete_all_previous_locations_query)
+
       for machine_dict in locations:
         machineID = machine_dict["MachineID"]
         x = machine_dict["location"]["x"]
         y = machine_dict["location"]["y"]
-        cursor.execute("UPDATE MachineLocation SET X_COORDINATE = %s, Y_COORDINATE = %s  WHERE MachineID = %s", (x,y,machineID,))
+        insert_new_location_query = "INSERT INTO MachineLocation(X_COORDINATE, Y_COORDINATE, MachineID) VALUES ((%s), (%s), (%s))"
+        cursor.execute(insert_new_location_query, (x,y,machineID,))
+        cursor.close()
+        connection.commit()
+        return jsonify("Successfully updated machine locations"), 202
     else:
-      return (jsonify("You are NOT a TA!"), 400)
-    connection.commit()
-    cursor.close()
-    connection.close()
-    return jsonify([])
+      return (jsonify("You are NOT a TA! Only TAs can update machine locations"), 400)
