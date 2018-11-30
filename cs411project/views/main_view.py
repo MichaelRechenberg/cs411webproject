ae95c89f8cac854cfd0bca5dbc9d5b8f0d374bfb
ThomasV14 <Thomas@wirelessprv-10-194-134-71.near.illinois.edu>
2018-11-29 22:02:37 -0600

    Fixed Main View

--- cs411project/views/main_view.py
+++ cs411project/views/main_view.py
@@ -4,8 +4,7 @@ from flask import Flask, render_template, session, redirect, url_for
 class mainView(View):
 
     def dispatch_request(self):
-        # if('netId' in session):
-        #     return render_template('mainView.html', netId=session['netId'])
-        # else:
-        #     return redirect(url_for('loginPage'))
-        return render_template('mainView.html',netId='hop2')
+        if('netId' in session):
+            return render_template('mainView.html', netId=session['netId'])
+        else:
+            return redirect(url_for('loginPage'))
